6b4495dc55d49244fae0b15b2c664d486a96b854
Vega <rubi@maniak.com.mx>
2020-08-25 00:27:51 -0500

    Add Yowi Layout Page

---
+++ src/components/Divider.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="divider"></div>
+</template>
+
+<script>
+    export default {
+        name: "Divider"
+    }
+</script>
+
+<style scoped>
+.divider {
+    height: 1px;
+    background-image: linear-gradient(90deg,  var(--red) 0%, var(--gorse) 100%);
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header  class="header logo">
+      <a href="https://yowi.tv/" >
+          <picture class="logo">
+              <source media="(min-width: 900px)" srcset="https://yowi.tv/img/yowiLogo.bd3c2f1a.svg">
+              <source media="(max-width: 480px)" srcset="https://yowi.tv/img/03.90d40a14.svg">
+              <img src="https://yowi.tv/img/yowiLogo.bd3c2f1a.svg" alt="Yowi logo"/>
+          </picture>
+      </a>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+    .header {
+    padding: 1rem;
+    }
+    .logo {
+        width: 12.5em;
+    }
+</style>
